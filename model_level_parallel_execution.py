2d4e690c120bc60179948f02b3064e78b1cbfda9
Timur Khairulov <khairulov00@gmail.com>
2025-07-03 20:43:59 +0900

    add multi-gpu, no logging, save to csv by row index

--- model_level_parallel_execution.py
+++ model_level_parallel_execution.py
@@ -9,6 +9,7 @@ from mlflow.exceptions import MlflowException
 from TALENT.model.utils import get_deep_args, get_method, set_seeds
 from TALENT.model.lib.data import get_dataset
 import sys
+from contextlib import nullcontext
 
 
 def fold_run_exists(experiment_name, fold):
@@ -29,7 +30,11 @@ def process_model_on_gpus(gpu_ids, model, experimental_setup):
     visible = ",".join(map(str, gpu_ids))
     os.environ["CUDA_VISIBLE_DEVICES"] = visible
     print(f"[GPUs {visible}] Running model: {model['name']}")
-    
+
+    log_to_mlflow = experimental_setup.get("log_to_mlflow", True)  # <- default: log
+    if not log_to_mlflow:
+        print("!!!!!!!!!!!!!!!!!!!!!!!!! WARNING: MLFLOW LOG DISABLED RUN !!!!!!!!!!!!!!!!!!!!!!!!!")
+
     model_name = model["name"]
     indices_models = {"t2gformer", "realmlp", "grande", "amformer", "tabm", "trompt", "bishop", 'PFN-v2'}
     tabr_ohe_models = {"mlp_plr", "tabr", "modernNCA"}
@@ -40,24 +45,35 @@ def process_model_on_gpus(gpu_ids, model, experimental_setup):
         clean_dataset = dataset['name'].replace("/", "_").replace("\\", "_")
         clean_model_name = model_name.replace("/", "_").replace("\\", "_")
         experiment_name = f"{clean_dataset}-{clean_model_name}-{experimental_setup['max_epoch']}-{experimental_setup['cross_val_count']}-NEW"
-        mlflow.set_experiment(experiment_name)
+        
+        if log_to_mlflow:
+            mlflow.set_experiment(experiment_name)
+        
         metrics_across_folds = None
 
         for fold in tqdm(range(100, 100 + experimental_setup["cross_val_count"]), desc=f"{dataset['name']}-{model_name} folds"):
-            if fold_run_exists(experiment_name, fold):
-                print(f"[GPU {gpu_id}] FOLD {fold} IN {experiment_name} FOUND. SKIPPING...")
+            if fold_run_exists(experiment_name, fold) and not log_to_mlflow:
+                print(f"[GPU {gpu_ids[0]}] FOLD {fold} IN {experiment_name} FOUND. SKIPPING...")
                 continue
 
-            with mlflow.start_run(run_name=f"fold_{fold}") as child_run:
+            if log_to_mlflow:
+                run_context = mlflow.start_run(run_name=f"fold_{fold}")
+            else:
+                run_context = nullcontext()  # Use from contextlib for no-op context
+
+            with run_context:
                 args_list = [
                     "--dataset", dataset['name'],
                     "--dataset_path", "data",
                     "--max_epoch", str(experimental_setup["max_epoch"]),
                     "--model_type", model_name,
                     "--batch_size", str(batch_size),
-                    "--gpu", str(gpu_ids),
+                    "--gpu", gpu_ids[0],
                     "--cross_val_fold", str(fold),
                 ]
+
+                if len(gpu_ids) > 1:
+                    args_list += ["--multiple_gpu", visible]
 
                 if model_name in indices_models:
                     args_list += ["--cat_policy", "indices"]
@@ -67,27 +83,40 @@ def process_model_on_gpus(gpu_ids, model, experimental_setup):
                     args_list += ["--normalization", "none"]
 
                 args, _, _ = get_deep_args(args_list)
+                print("ARGS")
+                print(args)
 
-                for k, v in vars(args).items():
-                    try:
-                        mlflow.log_param(k, v)
-                    except MlflowException:
-                        pass
-
-                mlflow.log_param("fold", fold)
-                mlflow.log_param("model", model_name)
-                mlflow.log_param("gpu_ids", visible)
+                if log_to_mlflow:
+                    for k, v in vars(args).items():
+                        try:
+                            mlflow.log_param(k, v)
+                        except MlflowException:
+                            pass
+                    mlflow.log_param("fold", fold)
+                    mlflow.log_param("model", model_name)
+                    mlflow.log_param("gpu_ids", visible)
 
                 set_seeds(experimental_setup["seed"])
+                print("---------> LOADING THE DATASET")
                 train_val_data, test_data, info = get_dataset(args.dataset, args.dataset_path, fold)
+                print("---------> LOADING THE MODEL")
                 method = get_method(args.model_type)(args, info["task_type"] == "regression")
+                print("---------> TRAINING THE MODEL")
                 time_cost = method.fit(train_val_data, info)
                 num_fea = train_val_data[0]['train'].shape[1] if train_val_data[0] else train_val_data[1]['train'].shape[1]
-                mlflow.log_param("num_features", num_fea)
+                
+                if log_to_mlflow:
+                    mlflow.log_param("num_features", num_fea)
 
-                start_time = time.time()
-                vl, vres, metric_name, _ = method.predict(test_data, info, model_name=args.evaluate_option)
-                elapsed = time.time() - start_time
+                save_results_to_csv = experimental_setup.get("save_results_to_csv", False)  # <- default: log
+                if not save_results_to_csv:
+                    start_time = time.time()
+                    vl, vres, metric_name, _ = method.predict(test_data, info, model_name=args.evaluate_option)
+                    elapsed = time.time() - start_time
+                else:
+                    start_time = time.time()
+                    vl, vres, metric_name, _ = method.predict_and_safe_to_file(test_data, info, model_name=args.evaluate_option)
+                    elapsed = time.time() - start_time
 
                 if metrics_across_folds is None:
                     metrics_across_folds = {name: [] for name in metric_name}
@@ -97,11 +126,12 @@ def process_model_on_gpus(gpu_ids, model, experimental_setup):
                     metrics_across_folds[name].append(val)
                 metrics_across_folds['time'].append(time_cost)
 
-                mlflow.log_metric("val_loss", vl)
-                for name, val in zip(metric_name, vres):
-                    mlflow.log_metric(name, val)
-                mlflow.log_metric("train_time", time_cost)
-                mlflow.log_metric("inference_time", elapsed)
+                if log_to_mlflow:
+                    mlflow.log_metric("val_loss", vl)
+                    for name, val in zip(metric_name, vres):
+                        mlflow.log_metric(name, val)
+                    mlflow.log_metric("train_time", time_cost)
+                    mlflow.log_metric("inference_time", elapsed)
 
 
 def distribute_models_among_gpus(models, gpus):
